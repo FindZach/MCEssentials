289428bb88f65b73f5e964ff3ae7549fb9ce4842
FindZach <zach@findzach.com>
2023-09-09 22:25:05 -0400

    added basic MCEssentialsConstant.java and Lombok

---
+++ src/main/java/com/findzach/mcessentials/MCEssentialsConstant.java
@@ -0,0 +1,16 @@
+package com.findzach.mcessentials;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.bukkit.Location;
+
+/**
+ * @author Zach S <zach@findzach.com>
+ * @since 9/9/2023
+ */
+@Getter
+@Setter
+public class MCEssentialsConstant {
+    private static Location spawn;
+    private static Location firstSpawn;
+}
