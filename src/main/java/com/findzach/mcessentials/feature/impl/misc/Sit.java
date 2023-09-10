dc6e85e5faec622cfde1dd4ff144b4e50a346c61
FindZach <zach@findzach.com>
2023-09-10 17:51:57 -0400

    Optimized sit logic

--- src/main/java/com/findzach/mcessentials/feature/impl/misc/Sit.java
+++ src/main/java/com/findzach/mcessentials/feature/impl/misc/Sit.java
@@ -4,6 +4,10 @@ import com.findzach.mcessentials.feature.Feature;
 import com.findzach.mcessentials.feature.FeatureType;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.Directional;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -49,18 +53,67 @@ public class Sit extends Feature {
         // Check if the action is right click
         if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
             // Check if the clicked block is a stair
-            Material clickedType = event.getClickedBlock().getBlockData().getMaterial();
-            if (isStair(clickedType)) {
-                Location sitLocation = event.getClickedBlock().getLocation().add(0.5, -0.4, 0.5); // Adjust these numbers as needed
-                ArmorStand armorStand = player.getWorld().spawn(sitLocation, ArmorStand.class);
+            Block clickedBlock = event.getClickedBlock();
+            Material clickedType = clickedBlock.getBlockData().getMaterial();
 
-                armorStand.setGravity(false);
-                armorStand.setVisible(false);
-                armorStand.setSmall(true);
-                armorStand.addPassenger(player);
+            if (isStair(clickedType)) {
+                Location sitLocation = getSitLocation(clickedBlock);
+
+                if (sitLocation != null) {
+                    ArmorStand armorStand = player.getWorld().spawn(sitLocation, ArmorStand.class);
+
+                    armorStand.setGravity(false);
+                    armorStand.setVisible(false);
+                    armorStand.setSmall(true);
+                    armorStand.addPassenger(player);
+                }
+            }
+        }
+
+    }
+
+    private Location getSitLocation(Block block) {
+        BlockData blockData = block.getBlockData();
+        Location blockLocation = block.getLocation();
+        double offsetX = 0.0;
+        double offsetY = -0.6;
+        double offsetZ = 0.0;
+
+        if (blockData instanceof Directional) {
+            Directional directional = (Directional) blockData;
+            BlockFace facing = directional.getFacing();
+
+            System.out.println("Block Direction: " + facing);
+            switch (facing) {
+                case SOUTH:
+                    // Adjust offsets for south-facing stairs
+                    offsetX = 0.5;
+                    offsetY = -0.6;
+                    offsetZ = 0.3;
+                    break;
+                case NORTH:
+                    // Adjust offsets for north-facing stairs
+                    offsetX = 0.5;
+                    offsetY = -0.6;
+                    offsetZ = 0.7;
+                    break;
+                case EAST:
+                    // Adjust offsets for east-facing stairs
+                    offsetX = 0.3;
+                    offsetY = -0.6;
+                    offsetZ = 0.5;
+                    break;
+                case WEST:
+                    // Adjust offsets for west-facing stairs
+                    offsetX = 0.7;
+                    offsetY = -0.6;
+                    offsetZ = 0.5;
+                    break;
             }
 
         }
+
+        return blockLocation.add(offsetX, offsetY, offsetZ);
     }
 
     private List<String> approvedStairList = new ArrayList<>();
